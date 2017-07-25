7c882b02c68b855553449bbc0214197e9512202e
uralic <basnetprem1234@gmail.com>
2017-07-25 15:00:41 +0545

    Add files via upload

---
+++ fonts.sh
@@ -0,0 +1,16 @@
+sudo apt-cache search ttf-mscorefonts-installer
+sudo apt-get install ttf-mscorefonts-installer
+sudo apt-get install ttf-liberation
+sudo apt-get install fonts-liberation
+sudo apt-get install ttf-uralic
+sudo apt-get install fonts-uralic
+sudo apt-get install ttf-root-installer
+apt-get install ttf-freefont
+sudo apt-get install ttf-dustin
+sudo apt-get install ttf-linux-libertine
+sudo apt-get install fonts-linuxlibertine
+sudo apt-get install fonts-dustin
+sudo apt-get install ttf-staypuft
+sudo fc-cache -fv
+sudo fc-list
+sudo dpkg-reconfigure fontconfig-config
