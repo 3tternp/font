03a31a955f97c69033dd4a30a7e6d86622fd5e27
astra <basnetprem1234@gmail.com>
2017-08-15 09:33:21 +0545

    Update fonts.sh

--- fonts.sh
+++ fonts.sh
@@ -5,7 +5,6 @@ sudo apt-get install fonts-liberation
 sudo apt-get install fonts-uralic
 sudo apt-get install ttf-root-installer
 apt-get install ttf-freefont
-sudo apt-get install ttf-linux-libertine
 sudo apt-get install fonts-linuxlibertine
 sudo apt-get install fonts-dustin
 sudo apt-get install ttf-staypuft
